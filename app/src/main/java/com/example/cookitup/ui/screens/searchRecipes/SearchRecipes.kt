ed5cc7b8b3ebc47ca634164796355e5d23365421
Luca Marchi <luca1010.marchi@gmail.com>
2025-04-29 17:54:44 +0200

    feat: add FlowRow to display ingredients

--- app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipes.kt
+++ app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipes.kt
@@ -1,14 +1,17 @@
 package com.example.cookitup.ui.screens.searchRecipes
 
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -17,7 +20,9 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.cookitup.network.ApiClient
 import kotlinx.coroutines.CoroutineScope
@@ -25,6 +30,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun SearchRecipes(
     state: SearchRecipesState,
@@ -33,13 +39,13 @@ fun SearchRecipes(
     Scaffold { paddingValues ->
         Column(modifier = Modifier.padding(paddingValues).fillMaxSize()) {
             var text by remember { mutableStateOf("") }
-            Row {
+            Row(verticalAlignment = Alignment.CenterVertically) {
                 OutlinedTextField(
                     value = text,
                     onValueChange = { text = it },
                     placeholder = { Text("Enter an ingredient") }
                 )
-                Button(
+                OutlinedButton(
                     onClick = {
                         actions.addIngredient(text)
                         text = ""
@@ -51,9 +57,22 @@ fun SearchRecipes(
             }
             Spacer(modifier = Modifier.padding(20.dp))
 
-            LazyColumn {
-                items(state.ingredients) { ingredient ->
-                    Text(ingredient)
+            FlowRow(
+                modifier = Modifier.padding(8.dp)
+            ) {
+                state.ingredients.forEach { ingredient ->
+                    Box(
+                        modifier = Modifier
+                            .padding(vertical = 4.dp, horizontal = 4.dp)
+                            .border(
+                                width = 1.dp,
+                                color = Color.Gray,
+                                shape = RoundedCornerShape(8.dp)
+                            )
+                            .padding(12.dp)
+                    ) {
+                        Text(text = ingredient)
+                    }
                 }
             }
         }
