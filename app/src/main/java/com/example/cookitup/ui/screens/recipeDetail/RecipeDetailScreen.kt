9150a2b10d3678f52c6391c9a0b4ab9b101c59a6
Luca Marchi <luca1010.marchi@gmail.com>
2025-05-06 11:41:55 +0200

    feat: add detail card

--- app/src/main/java/com/example/cookitup/ui/screens/recipeDetail/RecipeDetailScreen.kt
+++ app/src/main/java/com/example/cookitup/ui/screens/recipeDetail/RecipeDetailScreen.kt
@@ -1,15 +1,24 @@
 package com.example.cookitup.ui.screens.recipeDetail
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import coil.compose.AsyncImage
 
 @Composable
 fun RecipeDetail(
@@ -30,9 +39,39 @@ fun RecipeDetail(
                 is RecipeDetailState.Loading -> CircularProgressIndicator(
                     modifier = Modifier.align(Alignment.CenterHorizontally)
                 )
-                is RecipeDetailState.Success -> TODO()
-                is RecipeDetailState.Error -> TODO()
+                is RecipeDetailState.Success -> RecipeInfo(state)
+                is RecipeDetailState.Error -> Text(
+                    text = state.message,
+                    color = Color.Red,
+                    modifier = Modifier.align(Alignment.CenterHorizontally)
+                )
             }
+        }
+    }
+}
+
+@Composable
+fun RecipeInfo(state: RecipeDetailState.Success) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            AsyncImage(
+                model = state.detail.image,
+                contentDescription = "Image for ${state.detail.title}",
+                modifier = Modifier.fillMaxWidth()
+            )
+            Text(state.detail.title, style = MaterialTheme.typography.titleLarge)
+            Text("Time to cook: ${state.detail.time}", style = MaterialTheme.typography.bodyMedium)
+            Text("Servings: ${state.detail.servings}", style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
