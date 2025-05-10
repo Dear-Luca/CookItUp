c359f5c0b19162ee7e76f19a520687b235bfa46c
Luca Marchi <luca1010.marchi@gmail.com>
2025-05-10 15:58:36 +0200

    fix: style

--- app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipesScreen.kt
+++ app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipesScreen.kt
@@ -1,9 +1,7 @@
 package com.example.cookitup.ui.screens.searchRecipes
 
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
@@ -21,7 +19,6 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -33,7 +30,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.example.cookitup.ui.Routes
@@ -130,10 +126,9 @@ fun SearchRecipes(
                     Spacer(modifier = Modifier.padding(horizontal = 4.dp))
                     Icon(
                         imageVector = Icons.Default.Search,
-                        contentDescription = "Search ingredient",
+                        contentDescription = "Search ingredient"
                     )
                 }
-
             }
         }
     }
