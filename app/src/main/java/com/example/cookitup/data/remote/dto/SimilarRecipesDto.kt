7cd638fb3f6b4b359364b966e25aa65c2bec66f4
Luca Marchi <luca1010.marchi@gmail.com>
2025-05-25 19:24:27 +0200

    feat: Similar recipe dto

---
+++ app/src/main/java/com/example/cookitup/data/remote/dto/SimilarRecipesDto.kt
@@ -0,0 +1,8 @@
+package com.example.cookitup.data.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class SimilarRecipesDto(
+    @SerializedName("id") val id: Long,
+    @SerializedName("title") val title: String?
+)
