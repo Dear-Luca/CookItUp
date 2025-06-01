d4b01ffa564924ae2a1361936b337d671cfecfc0
Luca Marchi <luca1010.marchi@gmail.com>
2025-06-01 21:00:21 +0200

    chore: supabase dependencies

--- app/src/main/java/com/example/cookitup/data/remote/api/ApiConstants.kt
+++ app/src/main/java/com/example/cookitup/data/remote/api/ApiConstants.kt
@@ -5,5 +5,6 @@ import com.example.cookitup.BuildConfig
 const val BASE_URL = "https://api.spoonacular.com/"
 const val SPOONACULAR_API_KEY = BuildConfig.SPOONACULAR_API_KEY
 const val SUPABASE_API_KEY = BuildConfig.SUPABASE_API_KEY
+const val SUPABASE_URL = "https://isdawhtoaacrzsmzxuwi.supabase.co"
 const val NUM_ITEMS = 10
 const val IMAGES_ENDPOINT = "https://img.spoonacular.com/recipes/"
