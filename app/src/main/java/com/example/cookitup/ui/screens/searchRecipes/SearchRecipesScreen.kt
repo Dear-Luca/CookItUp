6cc1a383b59fe20d7ae590ca67f98fa5274f11a0
Luca Marchi <luca1010.marchi@gmail.com>
2025-06-29 12:17:19 +0200

    fix: scrollable

--- app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipesScreen.kt
+++ app/src/main/java/com/example/cookitup/ui/screens/searchRecipes/SearchRecipesScreen.kt
@@ -21,8 +21,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Close
@@ -43,8 +45,6 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -55,7 +55,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -80,16 +79,16 @@ fun SearchRecipes(
 ) {
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
+//    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
     val context = LocalContext.current
     var text by remember { mutableStateOf("") }
 
     Scaffold(
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+//        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         snackbarHost = {
             SnackbarHost(hostState = snackbarHostState)
         },
-        topBar = { TopBar(navController, stringResource(R.string.title_home), scrollBehavior) },
+        topBar = { TopBar(navController, stringResource(R.string.title_home)) },
         bottomBar = { BottomBar(navController) }
     ) { paddingValues ->
         Box(
@@ -100,7 +99,8 @@ fun SearchRecipes(
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(20.dp),
+                    .padding(20.dp)
+                    .verticalScroll(rememberScrollState()),
                 verticalArrangement = Arrangement.spacedBy(28.dp)
             ) {
                 // Header
@@ -233,8 +233,6 @@ fun SearchRecipes(
                         }
                     }
                 }
-
-                Spacer(modifier = Modifier.weight(1f))
 
                 AnimatedVisibility(
                     visible = state.ingredients.isNotEmpty(),
