ab4eaa314b079b32cc4c67ba77ea5e24916832a6
Luca Marchi <luca1010.marchi@gmail.com>
2025-07-13 12:59:55 +0200

    fix: scroll behaviour

--- app/src/main/java/com/example/cookitup/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/example/cookitup/ui/screens/profile/ProfileScreen.kt
@@ -3,6 +3,8 @@ package com.example.cookitup.ui.screens.profile
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -58,6 +60,7 @@ fun Profile(
     }
 
     Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopBar(
@@ -75,11 +78,13 @@ fun Profile(
                 scrollBehavior = scrollBehavior
             )
         },
-        bottomBar = { BottomBar(navController) },
-        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+        bottomBar = { BottomBar(navController) }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding).fillMaxSize()
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
             when (state) {
                 is ProfileState.Loading ->
